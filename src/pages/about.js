ff237f04e989a84ede43b79888c43c5eb07fd7a0
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2020-02-20 18:16:56 +0530

    Add files via upload

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function about() {
+    return (
+        <div>
+           <h1>
+               
+        About Us      
+        </h1> 
+
+
+        <p>
+            About Us Page
+        </p>
+        </div>
+    )
+}
